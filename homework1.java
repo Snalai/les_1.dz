d838732cbb28b9096756da7f7fba8835d527b7ce
Alan <perrefsdd@mail.ru>
2023-06-17 09:11:39 +0300

    les_1.dz

---
+++ homework1.java
@@ -0,0 +1,30 @@
+package org.example.lesson1.HomeWork;
+
+import java.util.Scanner;
+
+
+public class homework1 {
+    public static void main(String[] args) {
+        System.out.println("Введите текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String t = scanner.nextLine();
+        System.out.println("Было = " + t);
+        System.out.println("Стало = " + domzadanie1(t));
+    }
+
+    /**
+     * При наличии входной строки измените порядок слов на противоположный.
+     *
+     * @param t входная строка
+     * @return изменяет порядок слов на противоположный
+     */
+    private static String domzadanie1(String t) {
+        t.trim();
+        String[] words = t.split(" ");
+        int length = 0;
+        String a = "";
+        for (String element : words) length++;
+        for (int i = length - 1; i >= 0; i--) a = a.trim() + " " + words[i];
+        return a;
+    }
+}
---
+++ homework2.java
@@ -0,0 +1,33 @@
+package org.example.lesson1.HomeWork;
+
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main(String[] args) {
+        System.out.println("Сколько желаете ввести чисел?");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Сумма положительных чисел, после которых следует отрицательное число = " + domzadanie2(n, scanner));
+    }
+
+    /**
+     * Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных чисел, после которых следует отрицательное число.
+     * Пример ввода:
+     * 1 2 1 2 -1 1 3 1 3 -1 0
+     * @param n длинна последовательности
+     * @param scanner вспомогательный класс
+     * @return сумму положительных чисел, после которых следует отрицательное число
+     */
+    private static int domzadanie2(int n,  Scanner scanner) {
+        int sum = 0;
+        int b = 0;
+        for (int i = 0; i < n/2; i++) {
+            int a = scanner.nextInt();
+            if (a < 0 ) sum += b;
+            b = scanner.nextInt();
+            if (b < 0 ) sum += a;
+
+        }
+        return sum;
+    }
+}
