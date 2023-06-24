437b70edae51db469cd480687f465ce24be60dd9
Alan <62291221+Snalai@users.noreply.github.com>
2023-06-24 19:27:12 +0300

    Update homework2.java

--- homework2.java
+++ homework2.java
@@ -1,48 +1,42 @@
-package org.example.lesson4.HomeWork;
+package org.example.lesson3.HomeWork.HomeWork2;
 
-public class HomeWork2 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        String str = "(";
-        System.out.println("homezadanie2(str) = " + homezadanie2(str));
-    }
-
-    /**
-     * @apiNote При наличии строки, содержащей только символы , , , , и , определите, является ли входная строка допустимой.s'('')''{''}''['']'
-     * @param str входная строка
-     * @return является  ли строка допустимой
-     */
-    private static boolean homezadanie2(String str) {
-        boolean b = false;
-        int f = 0;
-        if (str.length() > 1) {
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '(') {
-                    if (str.charAt(i + 1) == ')') {
-                        b = true;
-                    } else if (')' == str.charAt(str.length() - 1)) {
-                        b = true;
-                    } else f++;
+        HomeWork2 homeWork1 = new HomeWork2("яблоки", "Россия", 5, 6, 1);
+        HomeWork2 homeWork2 = new HomeWork2("яблоки", "Германия", 10, 20, 1);
+        HomeWork2 homeWork3 = new HomeWork2("груши", "Франция", 15, 30, 2);
+        HomeWork2 homeWork4 = new HomeWork2("персики", "Китай", 20, 50, 1);
+        List<HomeWork2> homeWork2List = new ArrayList<>();
+        homeWork2List.add(homeWork1);
+        homeWork2List.add(homeWork2);
+        homeWork2List.add(homeWork3);
+        homeWork2List.add(homeWork4);
+        Scanner scanner = new Scanner(System.in);
+        Integer s_grade = 0;
+        int min_price = 0;
+        String name_product = "";
+        System.out.println("Выберите сорт 1 или 2");
+        while (s_grade != 1 | s_grade != 2) {
+            s_grade = scanner.nextInt();
+            if (s_grade instanceof Integer & s_grade == 1 | s_grade == 2) {
+                for (int i = 0; i < homeWork2List.size(); i++) {
+                    if (s_grade == homeWork2List.get(i).getGrade()) {
+                        min_price = homeWork2List.get(i).getPrice();
+                        name_product = homeWork2List.get(i).getName();
+                    }
                 }
-                if (str.charAt(i) == '[') {
-                    if (str.charAt(i + 1) == ']') {
-                        b = true;
-                    } else if (']' == str.charAt(str.length() - 1)) {
-                        b = true;
-                    } else f++;
+                for (int i = 0; i < homeWork2List.size(); i++) {
+                    if (min_price > homeWork2List.get(i).getPrice() & s_grade == homeWork2List.get(i).getGrade()) {
+                        min_price = homeWork2List.get(i).getPrice();
+                        name_product = homeWork2List.get(i).getName();
+                    }
                 }
-                if (str.charAt(i) == '{') {
-                    if (str.charAt(i + 1) == '}') {
-                        b = true;
-                    } else if ('}' == str.charAt(str.length() - 1)) {
-                        b = true;
-                    } else f++;
-                }
-
-            }
+                System.out.print(name_product + " , " + min_price);
+            } else System.out.println("Вы не ввели 1 или 2");
         }
-        if (f > 0){
-            b = false;
-        }
-        return b;
     }
 }
