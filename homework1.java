d5953bf926df4dea265e119b229f682289a49dfe
Alan <62291221+Snalai@users.noreply.github.com>
2023-06-24 19:31:51 +0300

    Update homework1.java

--- homework1.java
+++ homework1.java
@@ -1,28 +1,22 @@
-package org.example.lesson3.HomeWork.HomeWork1;
+package org.example.lesson4.HomeWork;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.LinkedList;
 
-public class Main {
+public class HomeWork1 {
     public static void main(String[] args) {
-        HomeWork1 homeWork1 = new HomeWork1("Мясо курица высший", 5, 1);
-        HomeWork1 homeWork2 = new HomeWork1("Мясо говядина высший", 10, 2);
-        HomeWork1 homeWork3 = new HomeWork1("низший слон", 15, 2);
-        List<HomeWork1> homeWork1List = new ArrayList<>();
-        homeWork1List.add(homeWork1);
-        homeWork1List.add(homeWork2);
-        homeWork1List.add(homeWork3);
-        int h_price = homeWork1List.get(0).getPrice();
-        for (int i = 0; i < homeWork1List.size(); i++) {
-            if (homeWork1List.get(i).getName().contains("высший")
-                    & (homeWork1List.get(i).getGrade() == 1
-                    | homeWork1List.get(i).getGrade() == 2)) {
-                if (h_price < homeWork1List.get(i).getPrice()) {
-                    h_price = homeWork1List.get(i).getPrice();
-                }
-            }
-
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.add(1);
+        integerLinkedList.add(2);
+        integerLinkedList.add(3);
+        integerLinkedList.add(4);
+        System.out.println(integerLinkedList);
+        Collections.reverse(integerLinkedList);
+        System.out.println(integerLinkedList);
+        for (int i = 0; i < integerLinkedList.size() + 3; i++) {
+            System.out.print(integerLinkedList.pop());
         }
-        System.out.println(h_price);
+
+
     }
 }
