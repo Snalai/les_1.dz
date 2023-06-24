af8066084ea409dca1b7223a8351f4d4007a5b44
Alan <62291221+Snalai@users.noreply.github.com>
2023-06-24 16:59:06 +0300

    Update homework1.java

--- homework1.java
+++ homework1.java
@@ -1,29 +1,20 @@
-package org.example.lesson1.HomeWork;
+import java.util.Collections;
+import java.util.LinkedList;
 
-import java.util.Scanner;
-
-public class homework1 {
+public class HomeWork1 {
     public static void main(String[] args) {
-        System.out.println("Введите текст: ");
-        Scanner scanner = new Scanner(System.in);
-        String t = scanner.nextLine();
-        System.out.println("Было = " + t);
-        System.out.println("Стало = " + domzadanie1(t));
-    }
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.add(1);
+        integerLinkedList.add(2);
+        integerLinkedList.add(3);
+        integerLinkedList.add(4);
+        System.out.println(integerLinkedList);
+        Collections.reverse(integerLinkedList);
+        System.out.println(integerLinkedList);
+        for (int i = 0; i < integerLinkedList.size() + 3; i++) {
+            System.out.print(integerLinkedList.pop());
+        }
 
-    /**
-     * При наличии входной строки измените порядок слов на противоположный.
-     *
-     * @param t входная строка
-     * @return изменяет порядок слов на противоположный
-     */
-    private static String domzadanie1(String t) {
-        t.trim();
-        String[] words = t.split(" ");
-        int length = 0;
-        String a = "";
-        for (String element : words) length++;
-        for (int i = length - 1; i >= 0; i--) a = a.trim() + " " + words[i];
-        return a;
+
     }
 }
